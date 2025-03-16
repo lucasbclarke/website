d9fc8d1649daa5d58661a96861268d49260ea9f9
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-03-16 20:58:20 +1100

    bruh

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 20000) {
-        window.scroll(window.scrollX, 20000);
-    }
-});
+let randNum = Math.random()
+console.log(randNum); 
+
+if (randNum > 0.5) {
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 5000) {
+            window.scroll(window.scrollX, 0);
+        }
+    });
+}
